80d05acb6beff08b842043b30c1aa43c7a5d837b
Vasiliy Faronov <vfaronov@gmail.com>
2019-05-16 19:23:02 +0300

    Update tools/check_example.sh for Django 1.11+

--- tools/check_example.sh
+++ tools/check_example.sh
@@ -5,13 +5,10 @@ set -e
 test -e setup.py || { echo 'must run from repo root' >&2; exit 1; }
 
 cd example/
-outfile=$( mktemp --tmpdir XXX.out )
 
-# In Django before 1.10, ``manage.py check`` does not support ``--fail-level``,
-# so we have to process its output manually.
-./manage.py check >"$outfile" 2>&1
-cat "$outfile"      # Show to the user.
-grep -q 'identified no issues' "$outfile"
+./manage.py check --fail-level WARNING
+
+outfile=$(mktemp --tmpdir XXX.out)
 
 ./manage.py test >"$outfile" 2>&1 || true
 cat "$outfile"      # Show to the user.
